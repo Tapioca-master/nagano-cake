77de223ff9aeffd5f6035d198f6ae320a7e6186c
aki-nori <i.akinori09@gmail.com>
2019-12-20 08:51:17 +0000

    [Add]配送先（Address）のCRUD機能作成

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,16 +1,41 @@
 class AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @addresses = Address.all
+    if @address.save
+      redirect_to addresses_path
+    else
+      render action: :index
+    end
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render action: :edit
+    end
   end
 
-  def delete
+  def destroy
+    address = Address.find(params[:id])
+    if address.destroy
+      redirect_to addresses_path
+    end
+  end
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
   end
 end
