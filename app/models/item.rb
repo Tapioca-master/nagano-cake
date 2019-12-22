469ea51461bc2c26728881dd7b47b8869d3d4086
tarorin0519 <rin.ice.musica0519@gmail.com>
2019-12-22 11:17:07 +0000

    itemモデル,バリデート,itemコントローラnew,create

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,17 +8,10 @@ class Item < ApplicationRecord
 	validates :non_tax_price, presence: true
 	validates :genre_id, presence: true
 	validates :sales_status, presence: true
-	validates :image_id, presence: true
 
 	enum sales_status: {
 		販売中: 0,
 		売り切れ: 1
 	}
 
-	enum genre_id: {
-		ケーキ: 0,
-		プリン: 1,
-		焼き菓子: 2,
-		キャンディ: 3
-	}
 end
