12aafc1bbf8968a7e5d68e1fe1dc0d4e0c907296
maritlage2020 <maritlage2020.amana@gmail.com>
2019-12-23 14:24:46 +0900

    作成開始

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,17 @@
 class ItemsController < ApplicationController
   def index
+  	@items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
+    
+  end
+
+  def create
+  	@item = Item.find_by(params[:id])
+  	@customer = Customer.
+    redirect_to cart_item_path(current_user.id)
   end
 
   def about
