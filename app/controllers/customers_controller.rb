75b999c44184b8fc3522eb2eae6eae3aa1a79258
Keita Shimazaki <onini21@gmail.com>
2019-12-23 09:01:17 +0000

    マイページ、顧客情報編集ページ、カスタマーコントローラーが完成？

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,26 @@
 class CustomersController < ApplicationController
   def show
+  	@customer = Customer.find(params[:id])
   end
 
   def edit
+  	@customer = Customer.find(params[:id])
   end
 
   def update
+  	@customer = Customer.find(params[:id])
+  	if @customer.update(customer_params)
+       redirect_to customer_path(@customer)
+    else
+       render :edit
+    end
   end
 
   def exit
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:name_last, :name_first, :name_last_kana, :name_first_kana, :postal_code, :address, :phone_number, :email)
   end
 end
