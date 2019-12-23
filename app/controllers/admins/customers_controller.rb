b7339506e05ecc2a44d00aa5da88f2c72da7f6a1
aki-nori <i.akinori09@gmail.com>
2019-12-23 00:31:06 +0000

    [Add]管理者側のCustomer編集周り追加

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,16 +1,42 @@
 class Admins::CustomersController < ApplicationController
   def index
+    permission
+    @customers = Customer.all
   end
 
   def show
+    permission
+    @customer = Customer.find(params[:id])
   end
 
   def personal
   end
 
   def edit
+    permission
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    permission
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admins_customer_path(@customer)
+    else
+      render action: :edit
+    end
+  end
+
+  #admin権限の判定(aki)
+  def permission
+    unless current_admin
+      redirect_to new_admin_session_path
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :name_first, :name_last, :name_first_kana, :name_last_kana, :email, :postal_code, :address, :phone_number, :is_active)
   end
 end
