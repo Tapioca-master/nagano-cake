44197ca0129dacb9a04274a6870a5a6a1660b9a0
Keita Shimazaki <onini21@gmail.com>
2019-12-24 04:53:33 +0000

    C注文一覧

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
   def index
+<<<<<<< Updated upstream
+=======
+  	  @orders = Order.all
+  	  @order_items = OrderItem.where(customer_id: current_customer.id)
+>>>>>>> Stashed changes
   end
-
   def show
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
 	has_many :order_items, dependent: :destroy
 	belongs_to :customer
+
+	@total_price = Cost.sum(:orders.shipping)
 end
