9fbff917b60fe6f7573dc14b9922eb5a60bbf577
maritlage2020 <maritlage2020.amana@gmail.com>
2019-12-24 17:00:26 +0900

    cart_itemのindexを修正

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,15 +1,23 @@
 class CartItemsController < ApplicationController
   def index
-    @cart_items = CartItem.where(customer_id: current_customer.id)
+    # @cart_items = CartItem.where(customer_id: current_customer.id)
+    @cart_items = current_customer.cart_items
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-      if  @cart_item.save
-          redirect_to cart_items_path
+    if cart_exist?(params[:cart_item][:item_id].to_i)
+      cart_item  = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id].to_i)
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    else
+      @cart_item = CartItem.new(cart_item_params)
+      if @cart_item.save
+        redirect_to cart_items_path
       else
-          render item_path (@cart_item.item_id)
+        render item_path (@cart_item.item_id)
       end
+    end
   end
 
   def info
@@ -26,7 +34,10 @@ class CartItemsController < ApplicationController
   end
 
  private
-
+ def cart_exist?(item_id)
+  cart = CartItem.find_by(customer_id: current_customer.id, item_id: item_id)
+  cart.present?
+ end
  def cart_item_params
   params.require(:cart_item).permit(:amount, :item_id, :customer_id)
 
