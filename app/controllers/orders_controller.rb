3b4ff6ed94353e4a047f7618dde94a85b1b2192e
Keita Shimazaki <onini21@gmail.com>
2019-12-25 05:00:50 +0000

    order/showが完成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,10 @@
 class OrdersController < ApplicationController
   def index
   	  @orders = Order.where(customer_id:  current_customer.id)
-  	  @order_items = OrderItem.where(customer_id: current_customer.id)
+  	  @order_items = OrderItem.where(order_id: params[:id])
   end
   def show
+  	  @order = Order.find(params[:id])
+  	  @order_item = OrderItem.where(order_id: params[:id])
   end
 end
