e89bc082d60deeb847cd6fc8025be076b282dd21
aki-nori <i.akinori09@gmail.com>
2019-12-25 02:15:44 +0000

    [Add]Adminの注文管理周り作成

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -9,11 +9,19 @@ class Admins::OrdersController < ApplicationController
 
   def update
   	@order = Order.find(params[:id])
-  	if @order.update(order_item_params)
+  	if @order.update(order_params)
+  		# order_statusの変更に連動するproduction_statusの変更(aki)
+  		order_items = OrderItem.where(order_id: params[:id])
+  		binding.pry
+
+  		if @order.order_status.to_s == "入金確認" and order_items.find_by(production_status: :着手不可)
+  			order_items.update(production_status: :製作待ち)
+  		end
   		redirect_to admins_order_path(@order)
   	else
   		render action: :show
   	end
+
   end
 
   def show
@@ -21,10 +29,9 @@ class Admins::OrdersController < ApplicationController
   	@order_items = OrderItem.where(order_id: @order.id)
   end
 
-  private 
+  private
 
-  def order_item_params
+  def order_params
   	params.require(:order).permit(:order_status)
   end
-
 end
