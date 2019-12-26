f8035204575906c6b8637e5a37ab34caa3aeb74a
aki-nori <i.akinori09@gmail.com>
2019-12-26 00:09:21 +0000

    [Fix]Orderテーブルのorder_statusの型修正

--- db/migrate/20191219032639_create_orders.rb
+++ db/migrate/20191219032639_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
       t.integer :customer_id
-      t.string :order_status
+      t.integer :order_status
       t.integer :shipping
       t.string :ship_name
       t.string :ship_address
