7d28ee8641170d0f8e68bbb34c409685a466b073
aki-nori <i.akinori09@gmail.com>
2020-01-04 04:15:06 +0000

    [Fix]顧客新規登録時email以外のパラメータ取得しない症状修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+	# <ここから>Customer新規登録時のパラメタ取得
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name_first, :name_last, :name_first_kana, :name_last_kana, :postal_code, :address, :phone_number])
+    end
+    # <ここまで>
+
 end
