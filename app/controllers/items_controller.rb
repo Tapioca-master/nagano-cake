4263f5dd7f3aba7504e57bde6fccdb0d8e9a68ed
aki-nori <i.akinori09@gmail.com>
2020-01-06 04:02:02 +0000

    [Fix]ログインしていない場合にItemのShowがされない問題を修正、またShowではログインの有無によってカート遷移系を表示切り替えした。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_customer!, only: [:show]
+  # before_action :authenticate_customer!, only: [:show]
 
 
 	# TOPページkaminari表示数設定(aki)
